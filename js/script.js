b8ffcbec0b12e1f0c49611802302998f8d8184a2
matheusrodacki <matheus.rodacki@outlook.com>
2024-01-19 15:08:10 -0300

    fix: :art: change imgs folder name and add a canvas

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const graficoDolar = document.getElementById("graficoDolar");
+
+new Chart(graficoDolar, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
